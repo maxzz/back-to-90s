8a315073318862985f6630e4430b33e43d438a9c
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-13 14:06:39 -0700

    WIP. upgrade-2025. fix images import

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -1,8 +1,23 @@
+import { imagesApp } from "../store/images";
+import { ThatsAllFolks } from "./1-thats-all-folks";
+
 export function SectionMain() {
     return (
-        <div className="text-sm text-center text-balance">
-            <p>A long time ago I developed interfaces for a company that no longer exists, but the memories remain.</p>
-            <p>It was the dark age of square windows in Windows 95. Everything about the user interface was flat, square, and boring. I was lucky enough to convince the management to add a little fun, and it was the right decision at the time. I still love this design.</p>
+        <div className="relative text-sm text-center text-balance bg-slate-300">
+            <p>
+                A long time ago I developed interfaces for a company that no longer exists, but the memories remain.
+            </p>
+
+            <p>
+                It was the dark age of square windows in Windows 95.
+                Everything about the user interface was flat, square, and boring.
+                I was lucky enough to convince the management to add a little fun, and it was the right decision at the time.
+                I still love this design.
+            </p>
+
+            <img src={imagesApp[0]} alt="UI picture" />
+
+            <ThatsAllFolks className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fill-red-500" />
         </div>
     );
 }
--- src/components/store/images.tsx
+++ src/components/store/images.tsx
@@ -1,22 +1,21 @@
-import image01 from "~images/image001.png";
-import image02 from "~images/image002.png";
-import image03 from "~images/image003.png";
-import image04 from "~images/image004.png";
-import image05 from "~images/image005.png";
-import image06 from "~images/image006.png";
-import image07 from "~images/image007.png";
-import image08 from "~images/image008.png";
-import image09 from "~images/image009.png";
-import image10 from "~images/image010.png";
-import image11 from "~images/image011.png";
-import image12 from "~images/image012.png";
-import image13 from "~images/image013.png";
-import image14 from "~images/image014.png";
+import image01 from "@images/image001.png";
+import image02 from "@images/image002.png";
+import image03 from "@images/image003.png";
+import image04 from "@images/image004.png";
+import image05 from "@images/image005.png";
+import image06 from "@images/image006.png";
+import image08 from "@images/image008.png";
+import image09 from "@images/image009.png";
+import image10 from "@images/image010.png";
+import image11 from "@images/image011.png";
+import image12 from "@images/image012.png";
+import image13 from "@images/image013.png";
+import image14 from "@images/image014.png";
 
-import thumbnail01 from "~images/thumbnail001.png";
-import thumbnail02 from "~images/thumbnail002.png";
-import thumbnail03 from "~images/thumbnail003.png";
-import thumbnail04 from "~images/thumbnail004.png";
+import thumbnail01 from "@images/thumbnails_1.png";
+import thumbnail02 from "@images/thumbnails_2.png";
+import thumbnail03 from "@images/thumbnails_3.png";
+import thumbnail04 from "@images/thumbnails_4.png";
 
 export const imagesApp = [
     image01,
@@ -25,7 +24,6 @@ export const imagesApp = [
     image04,
     image05,
     image06,
-    image07,
     image08,
     image09,
     image10,
