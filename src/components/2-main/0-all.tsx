6adb6d1400e91c27687832dcdef9b4e3bf3eb5a6
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-13 15:12:59 -0700

    WIP. upgrade-2025. new main layout

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -3,21 +3,23 @@ import { ThatsAllFolks } from "./1-thats-all-folks";
 
 export function SectionMain() {
     return (
-        <div className="relative text-sm text-center text-balance bg-slate-300">
-            <p>
-                A long time ago I developed interfaces for a company that no longer exists, but the memories remain.
-            </p>
+        <div className="grid grid-cols-[1fr_minmax(100px,700px)_1fr] 1place-items-center gap-2 1bg-slate-300">
+            <div className="col-start-2 relative text-sm text-center text-balance bg-slate-300 grid justify-items-center grid-rows-[auto_1fr_auto] gap-2">
+                <p>
+                    A long time ago I developed interfaces for a company that no longer exists, but the memories remain.
+                </p>
 
-            <p>
-                It was the dark age of square windows in Windows 95.
-                Everything about the user interface was flat, square, and boring.
-                I was lucky enough to convince the management to add a little fun, and it was the right decision at the time.
-                I still love this design.
-            </p>
+                <p>
+                    It was the dark age of square windows in Windows 95.
+                    Everything about the user interface was flat, square, and boring.
+                    I was lucky enough to convince the management to add a little fun, and it was the right decision at the time.
+                    I still love this design.
+                </p>
 
-            <img src={imagesApp[0]} alt="UI picture" />
+                <img src={imagesApp[0]} alt="UI picture" />
 
-            <ThatsAllFolks className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fill-red-500" />
+                <ThatsAllFolks className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 fill-red-500" />
+            </div>
         </div>
     );
 }
