f6b071d900b19a36d1ace2fa3e15cad4e3cd71d9
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-12 19:34:40 -0700

    WIP. upgrade-2025. Initial clean up

--- src/components/0-app.tsx
+++ src/components/0-app.tsx
@@ -1,51 +1,9 @@
+import { SectionMain } from "./2-main";
+
 export function App() {
     return (
-        <div className="App">
-            <h1>Hello Vite + React!</h1>
-            <div className="card">
-                <button onClick={() => alert('Hello')}>Click me</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p>
-                <a
-                    className="link"
-                    href="https://vitejs.dev/guide/features.html"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Vite Docs
-                </a>
-                {' | '}
-                <a
-                    className="link"
-                    href="https://vitejs.dev/guide/api.html#cli"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Vite CLI
-                </a>
-            </p>
-            <p>
-                <a
-                    className="link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                {' | '}
-                <a
-                    className="link"
-                    href="https://vitejs.dev/guide/features.html"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Vite Docs
-                </a>
-            </p>
+        <div className="h-screen flex flex-col items-center justify-center">
+            <SectionMain />
         </div>
     );
 }
---
+++ src/components/2-main/0-all.tsx
@@ -0,0 +1,7 @@
+export function SectionMain() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1>Hello</h1>
+        </div>
+    );
+}
