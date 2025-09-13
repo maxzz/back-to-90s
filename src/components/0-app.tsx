2a23065002b982f1ae933b21ca216ef8dd648a6a
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-12 20:05:24 -0700

    WIP. upgrade-2025. app layout. 2.

--- src/components/0-app.tsx
+++ src/components/0-app.tsx
@@ -1,9 +1,13 @@
+import { Header } from "./1-header";
 import { SectionMain } from "./2-main";
+import { Footer } from "./3-footer";
 
 export function App() {
     return (
         <div className="h-screen flex flex-col items-center justify-center">
+            <Header />
             <SectionMain />
+            <Footer />
         </div>
     );
 }
