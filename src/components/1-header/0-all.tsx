74693618a1295d1da948a15a8923a047e18bc5af
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-13 13:42:40 -0700

    WIP. upgrade-2025. add ThatsAllFolks component

--- src/components/1-header/0-all.tsx
+++ src/components/1-header/0-all.tsx
@@ -4,6 +4,7 @@ export function Header({ className }: ComponentPropsWithoutRef<"header">) {
     return (
         <header className="p-2 flex items-center justify-center">
             <img className="pt-2 h-16" src="assets/images/title.svg" alt="title image of the 90's website" />
+
         </header>
     );
 }
