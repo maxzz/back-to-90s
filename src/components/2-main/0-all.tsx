d15d1396daaa8ea64c81495822cb7ff77cbfd598
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-12 19:59:31 -0700

    WIP. upgrade-2025. app layout. 1.

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -1,7 +1,8 @@
 export function SectionMain() {
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h1>Hello</h1>
+        <div className="">
+            <p>A long time ago I developed interfaces for a company that no longer exists, but the memories remain.</p>
+            <p>It was the dark age of square windows in Windows 95. Everything about the user interface was flat, square, and boring. I was lucky enough to convince the management to add a little fun, and it was the right decision at the time. I still love this design.</p>
         </div>
     );
 }
