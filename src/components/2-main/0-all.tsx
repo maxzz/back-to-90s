8e90f48f1ccb385b9db7387417459507bf924ce2
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-14 20:03:34 -0700

    WIP. upgrade-2025. explanation. 2

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -17,7 +17,13 @@ export function SectionMain() {
                         It was the dark age of square windows in Windows 95.
                         Everything about the user interface was flat, square, and boring.
 
-                        I was lucky enough to convince management to add some fun with skeuomorphism, and it was the right decision at the time.
+                        I was lucky enough to convince management to add some fun with
+                        {/* {' '}<a href="https://en.wikipedia.org/wiki/Skeuomorphism" target="_blank" rel="noreferrer"> */}
+                        {' '}<a href="https://www.google.com/search?q=skeuomorphism&sourceid=chrome&ie=UTF-8" target="_blank" rel="noreferrer">
+                            skeuomorphism
+                        </a>
+
+                        , and it was the right decision at the time.
                         {/* Skeuomorphism is a design approach where digital interfaces mimic real-world objects by replicating their textures, dimensions, and even their use, with the goal of making new technology more familiar and intuitive for users. */}
 
                         I still love this design.
