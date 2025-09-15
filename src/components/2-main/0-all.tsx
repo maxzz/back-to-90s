bb985b0d8ae5cf8cc8f269cee924e27c0ff69924
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-14 20:14:05 -0700

    WIP. upgrade-2025. explanation. 4

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -4,7 +4,7 @@ import { ThatsAllFolks } from "../ui/icons";
 
 export function SectionMain() {
     return (
-        <div className="grid grid-cols-[1fr_minmax(100px,700px)_1fr] bg-slate-300">
+        <div className="grid grid-cols-[1fr_minmax(100px,700px)_1fr] bg-slate-300 text-slate-700">
             <div className="col-start-2 text-xs grid grid-cols-[auto_1fr] gap-4 p-4">
                 <NavAppImages />
 
@@ -17,21 +17,26 @@ export function SectionMain() {
                         It was the dark age of square windows in Windows 95.
                         Everything about the user interface was flat, square, and boring.
 
-                        I was lucky enough to convince management to add some fun with
+                        I was lucky enough to convince the company's management to add some fun in the form of
                         {/* {' '}<a href="https://en.wikipedia.org/wiki/Skeuomorphism" target="_blank" rel="noreferrer"> */}
-                        {' '}<a href="https://www.google.com/search?q=skeuomorphism&sourceid=chrome&ie=UTF-8" target="_blank" rel="noreferrer">
-                            skeuomorphism
+                        {' '}
+                        <a href="https://www.google.com/search?q=skeuomorphism&sourceid=chrome&ie=UTF-8" target="_blank" rel="noreferrer">
+                            skeuomorphism.
                         </a>
-                        {/* Compared to flat design, skeuomorphic design seems to facilitate a fast navigation through graphic user interfaces, because icons are more easily recognized and less abstract than their minimalistic counterparts found in flat design. */}
-                        {/* Compared to flat design, skeuomorphic design appears to facilitate quick navigation through graphical user interfaces, as the icons are easier to recognize and less abstract than their minimalist counterparts found in flat design. */}
+                        {' '}
+                    </p>
 
-                        .
-
+                    <p>
                         Compared to flat design, skeuomorphic design seems to facilitate a fast navigation through graphic user interfaces, because icons are more easily recognized and less abstract than their minimalistic counterparts found in flat design.
+                        {/* Compared to flat design, skeuomorphic design appears to facilitate quick navigation through graphical user interfaces, as the icons are easier to recognize and less abstract than their minimalist counterparts found in flat design. */}
+                        {/* org: Compared to flat design, skeuomorphic design seems to facilitate a fast navigation through graphic user interfaces, because icons are more easily recognized and less abstract than their minimalistic counterparts found in flat design. */}
+
                         And it was the right decision at the time.
 
                         {/* Skeuomorphism is a design approach where digital interfaces mimic real-world objects by replicating their textures, dimensions, and even their use, with the goal of making new technology more familiar and intuitive for users. */}
+                    </p>
 
+                    <p>
                         I still love this design.
                     </p>
 
