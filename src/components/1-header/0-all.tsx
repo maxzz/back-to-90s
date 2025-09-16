43a34bf4fc102fe31b186cd06ace05cb510827d8
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-15 18:22:28 -0700

    WIP. upgrade-2025. integrate SvgForward

--- src/components/1-header/0-all.tsx
+++ src/components/1-header/0-all.tsx
@@ -1,10 +1,11 @@
 import { type ComponentPropsWithoutRef } from "react";
+import { SvgForward } from "../ui/images";
 
 export function Header({ className }: ComponentPropsWithoutRef<"header">) {
     return (
         <header className="p-2 flex items-center justify-center bg-slate-400">
-            <img className="pt-2 h-16" src="assets/images/title.svg" alt="title image of the 90's website" />
-
+            {/* <img className="pt-2 h-16" src="assets/images/title.svg" alt="title image of the 90's website" /> */}
+            <SvgForward className="pt-2 h-16" title="forward" titleId="forward" />
         </header>
     );
 }
--- src/components/ui/images/index.tsx
+++ src/components/ui/images/index.tsx
@@ -1,1 +1,1 @@
-export * from "./forward";
+export * from "./svg-forward";
