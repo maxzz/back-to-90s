baf0f1bb44fb91ffa3751998a35b7624112be154
Max Zakharzhevskiy <maxzz@msn.com>
2025-09-15 18:41:45 -0700

    WIP. upgrade-2025. code review

--- src/components/1-header/0-all.tsx
+++ src/components/1-header/0-all.tsx
@@ -1,9 +1,10 @@
 import { type ComponentPropsWithoutRef } from "react";
 import { SvgForward } from "../ui/images";
+import { classNames } from "@/utils";
 
-export function Header({ className }: ComponentPropsWithoutRef<"header">) {
+export function Header({ className, ...rest }: ComponentPropsWithoutRef<"header">) {
     return (
-        <header className="p-2 flex items-center justify-center bg-canvas border-b border-slate-600">
+        <header className={classNames("p-2 flex items-center justify-center bg-canvas border-b border-slate-600", className)} {...rest}>
             {/* <img className="pt-2 h-16" src="assets/images/title.svg" alt="title image of the 90's website" /> */}
             <SvgForward className="pt-2 h-16" title="forward" titleId="forward" />
         </header>
--- src/components/3-footer/0-all.tsx
+++ src/components/3-footer/0-all.tsx
@@ -1,8 +1,9 @@
+import { classNames } from "@/utils";
 import { type ComponentPropsWithoutRef } from "react";
 
-export function Footer({ className }: ComponentPropsWithoutRef<"header">) {
+export function Footer({ className, ...rest }: ComponentPropsWithoutRef<"header">) {
     return (
-        <footer className="p-2 text-xs text-center bg-canvas text-slate-400 border-t border-slate-600">
+        <footer className={classNames("p-2 text-xs text-center bg-canvas text-slate-400 border-t border-slate-600", className)} {...rest}>
             <a href="https://theoldnet.com/get?decode=false&scripts=false&year=1997&url=http://xirlink.com/vp.htm" target="_blank">
                 The 90's time machine website
             </a>
