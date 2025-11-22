9dddc90a8260ae94853f8de37568bfd110870f5e
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-21 19:12:27 -0800

    WIP. add add framer motion animation to CurrentImage when image appear it will scale from 0.5 to 1 and then change opacity from 0.5 to 1.

--- src/components/2-main/3-current-image.tsx
+++ src/components/2-main/3-current-image.tsx
@@ -1,11 +1,20 @@
-import { type ComponentPropsWithoutRef } from "react";
+import { motion, type HTMLMotionProps } from "framer-motion";
 import { useSnapshot } from "valtio";
 import { appSettings } from "../store/app-settings";
 import { imagesApp } from "../store/images";
 
-export function CurrentImage({ className, ...rest }: ComponentPropsWithoutRef<"img">) {
+export function CurrentImage({ className, ...rest }: HTMLMotionProps<"img">) {
     const { selectedImage } = useSnapshot(appSettings);
     return (
-        <img className={className} src={imagesApp[selectedImage]} alt="UI picture" {...rest} />
+        <motion.img
+            key={selectedImage}
+            initial={{ scale: 0.5, opacity: 0.5 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5 }}
+            className={className}
+            src={imagesApp[selectedImage]}
+            alt="UI picture"
+            {...rest}
+        />
     );
 }
